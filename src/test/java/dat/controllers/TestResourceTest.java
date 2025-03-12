68f329e1b64b1429653e9a9c3a112d0e25de3ec1
AndyTheDragon <andre.jansam@gmail.com>
2025-03-12 09:30:40 +0100

    Fixed hardcoded id in failing tests

--- src/test/java/dat/controllers/TestResourceTest.java
+++ src/test/java/dat/controllers/TestResourceTest.java
@@ -4,6 +4,7 @@ import dat.config.ApplicationConfig;
 import dat.config.HibernateConfig;
 import dat.dto.HotelDTO;
 import dat.entities.Hotel;
+import dat.entities.Room;
 import dat.routes.Routes;
 import io.restassured.RestAssured;
 import jakarta.persistence.EntityManager;
@@ -11,6 +12,8 @@ import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.*;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static io.restassured.RestAssured.given;
@@ -45,8 +48,10 @@ class TestResourceTest
             t1 = new Hotel("TestEntityA");
             t2 = new Hotel("TestEntityB");
             em.getTransaction().begin();
+            em.createQuery("DELETE FROM Room ").executeUpdate();
             em.createQuery("DELETE FROM Hotel ").executeUpdate();
-            em.createNativeQuery("ALTER SEQUENCE entity_id_seq RESTART WITH 1");
+            em.createNativeQuery("ALTER SEQUENCE room_id_seq RESTART WITH 1");
+            em.createNativeQuery("ALTER SEQUENCE hotel_id_seq RESTART WITH 1");
             em.persist(t1);
             em.persist(t2);
             em.getTransaction().commit();
@@ -57,22 +62,35 @@ class TestResourceTest
         }
     }
 
+    @AfterAll
+    void tearDown()
+    {
+        if (emf != null && emf.isOpen())
+        {
+            emf.close();
+            System.out.println("EntityManagerFactory closed");
+        }
+        ApplicationConfig.getInstance().stopServer();
+    }
+
     @Test
     void getAll()
     {
-        given().when().get("/entity").then().statusCode(200).body("size()", equalTo(2));
+        given().when().get("/hotel").then().statusCode(200).body("size()", equalTo(2));
     }
 
     @Test
     void getById()
     {
-        given().when().get("/entity/1").then().statusCode(200).body("id", equalTo(1));
+        given().when().get("/hotel/" + t2.getId()).then().statusCode(200).body("id", equalTo(t2.getId().intValue()));
     }
 
     @Test
     void create()
     {
-        Hotel entity = new Hotel("New entity");
+        Hotel entity = new Hotel("Thon Partner Hotel", "Carl Gustavs Gade 1");
+        Room room = new Room("201");
+        entity.addRoom(room);
         try
         {
             String json = objectMapper.writeValueAsString(new HotelDTO(entity));
@@ -80,10 +98,10 @@ class TestResourceTest
                     .contentType("application/json")
                     .accept("application/json")
                     .body(json)
-                    .post("/entity")
+                    .post("/hotel")
                     .then()
                     .statusCode(200)
-                    .body("title", equalTo("New entity")); // check here??
+                    .body("name", equalTo("Thon Partner Hotel")); // check here??
         } catch (JsonProcessingException e)
         {
             e.printStackTrace();
@@ -102,10 +120,10 @@ class TestResourceTest
                     .contentType("application/json")
                     .accept("application/json")
                     .body(json)
-                    .put("/entity/1") // double check id
+                    .put("/hotel/" + t1.getId()) // double check id
                     .then()
                     .statusCode(200)
-                    .body("title", equalTo("New entity2"));
+                    .body("name", equalTo("New entity2"));
         } catch (JsonProcessingException e)
         {
             e.printStackTrace();
