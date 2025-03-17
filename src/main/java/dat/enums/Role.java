0dc7b708e4c037adc4fc42212a9c66fe2284e850
AndyTheDragon <andre.jansam@gmail.com>
2025-03-17 21:56:04 +0100

    Consolidated routes in one class, moved Role to enum in separate file.

---
+++ src/main/java/dat/enums/Role.java
@@ -0,0 +1,10 @@
+package dat.enums;
+
+import io.javalin.security.RouteRole;
+
+public enum Role implements RouteRole
+{
+    ANYONE,
+    USER,
+    ADMIN
+}
--- src/main/java/dat/routes/Routes.java
+++ src/main/java/dat/routes/Routes.java
@@ -1,6 +1,9 @@
 package dat.routes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dat.controllers.HotelController;
+import dat.controllers.SecurityController;
+import dat.enums.Role;
 import io.javalin.apibuilder.EndpointGroup;
 import jakarta.persistence.EntityManagerFactory;
 import org.slf4j.Logger;
@@ -11,20 +14,49 @@ import static io.javalin.apibuilder.ApiBuilder.*;
 public class Routes
 {
     private static HotelController hotelController;
+    private static SecurityController securityController;
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
     private static final Logger logger = LoggerFactory.getLogger(Routes.class);
 
     public static EndpointGroup getRoutes(EntityManagerFactory emf)
     {
         hotelController = new HotelController(emf);
+        securityController = new SecurityController(emf);
         return () -> {
-            path("hotel", () -> {
-                get(hotelController::getAll);
-                post(hotelController::create);
-                get("/{id}", hotelController::getById);
-                put("/{id}", hotelController::update);
-                delete("/{id}", hotelController::delete);
-                get("{id}/rooms", hotelController::getRooms);
-            });
+            path("hotel", hotelRoutes());
+            path("auth", authRoutes());
+            path("protected", protectedRoutes());
+        };
+    }
+
+    private static EndpointGroup hotelRoutes()
+    {
+        return () -> {
+            get(hotelController::getAll);
+            post(hotelController::create);
+            get("/{id}", hotelController::getById);
+            put("/{id}", hotelController::update);
+            delete("/{id}", hotelController::delete);
+            get("{id}/rooms", hotelController::getRooms);
+        };
+    }
+
+    private static EndpointGroup authRoutes()
+    {
+        return () -> {
+            get("/test", ctx->ctx.json(jsonMapper.createObjectNode().put("msg",  "Hello from Open")), Role.ANYONE);
+            post("/login", securityController.login(), Role.ANYONE);
+            post("/register", securityController.register(), Role.ANYONE);
+            get("/verify", securityController.verify() , Role.ANYONE);
+            get("/tokenlifespan", securityController.timeToLive() , Role.ANYONE);
+        };
+    }
+
+    private static EndpointGroup protectedRoutes()
+    {
+        return () -> {
+            get("/user_demo",(ctx)->ctx.json(jsonMapper.createObjectNode().put("msg",  "Hello from USER Protected")), Role.USER);
+            get("/admin_demo",(ctx)->ctx.json(jsonMapper.createObjectNode().put("msg",  "Hello from ADMIN Protected")), Role.ADMIN);
         };
     }
 
