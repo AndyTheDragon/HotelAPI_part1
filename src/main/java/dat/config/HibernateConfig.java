17bdf48652ec4c161c95a18be2c97ac4ec3ca6e7
AndyTheDragon <andre.jansam@gmail.com>
2025-03-18 22:28:40 +0100

    Setup for GitHub Actions automatic deployment

--- src/main/java/dat/config/HibernateConfig.java
+++ src/main/java/dat/config/HibernateConfig.java
@@ -79,15 +79,15 @@ public class HibernateConfig
         props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
         props.put("hibernate.hbm2ddl.auto", "create");  // set to "update" when in production
         props.put("hibernate.current_session_context_class", "thread");
-        props.put("hibernate.show_sql", "true");
-        props.put("hibernate.format_sql", "true");
+        props.put("hibernate.show_sql", "false");
+        props.put("hibernate.format_sql", "false");
         props.put("hibernate.use_sql_comments", "false");
         return props;
     }
 
     private static Properties setDeployedProperties(Properties props) {
         String DBName = System.getenv("DB_NAME");
-        props.setProperty("hibernate.connection.url", System.getenv("CONNECTION_STR") + DBName);
+        props.setProperty("hibernate.connection.url", String.format(System.getenv("CONNECTION_STR"), DBName));
         props.setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"));
         props.setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"));
         return props;
