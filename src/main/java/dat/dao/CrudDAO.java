c06a1a03d9fcdc681ee4895d3771b2d14f29ad29
AndyTheDragon <andre.jansam@gmail.com>
2025-03-21 13:39:31 +0100

    Fixed style

--- src/main/java/dat/dao/CrudDAO.java
+++ src/main/java/dat/dao/CrudDAO.java
@@ -7,14 +7,12 @@ public interface CrudDAO
     <T> T create(T object);
     <T> List<T> create(List<T> objects);
 
+    <T> T getById(Class<T> type, Object id);
+    <T> List<T> getAll(Class<T> type);
+
     <T> T update(T object);
     <T> List<T> update(List<T> objects);
 
-    <T> T read(Class<T> type, Object id);
-
-    <T> List<T> findAll(Class<T> type);
-
     <T> void delete(T object);
-
     <T> void delete(Class<T> type, Object id);
 }
